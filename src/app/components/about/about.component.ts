0cf07ff83826ba190008dfa3c6396ca9abb30dbc
Velid-387 <velid7jelovac@gmail.com>
2025-04-06 22:18:44 +0200

    Implement translation support across multiple components, including About, Contact, Services, and Service Item components. Update HTML templates to utilize translation directives and enhance localization. Add corresponding translation keys to the translations file for improved internationalization.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateDirective } from '../../shared/directives/translate.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
